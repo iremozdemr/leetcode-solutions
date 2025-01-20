68a58d93ab4560b08ee3b759286b1db1c15ade04
İrem Özdemir <iremozdemirwww3@gmail.com>
2025-01-20 22:10:44 +0300

    Time: 1884 ms (5.01%) | Memory: 40.8 MB (18.75%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -13,30 +13,6 @@ class Solution {
             }
         }
 
-        /*
-        if((num/16)*(num/16) < num){
-            for(int i=(num/16)-1; i <= num/2; i++){
-                if(i * i == num){
-                    return true;
-                }
-            }
-        }
-        else if((num/4)*(num/4) < num){
-            for(int i=(num/4)-1; i <= num/2; i++){
-                if(i * i == num){
-                    return true;
-                }
-            }
-        }
-        else{
-            for(int i=0; i <= num/2; i++){
-                if(i * i == num){
-                    return true;
-                }
-            }
-        }
-        */
-
         return false;
     }
 }
