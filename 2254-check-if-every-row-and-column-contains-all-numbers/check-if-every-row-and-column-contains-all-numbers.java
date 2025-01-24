bb326d6dfd9bf29f569c2f5cf6edc9fd89a12d99
İrem Özdemir <iremozdemirwww3@gmail.com>
2025-01-25 00:59:27 +0300

    Time: 15 ms (73.25%) | Memory: 45.6 MB (49.23%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.java
@@ -4,9 +4,6 @@ class Solution {
             int[] array = new int[matrix.length];
             for(int i=0; i<matrix.length; i++){
                 array[i] = matrix[i][j];
-                System.out.println("row " + i);
-                System.out.println("column " + j);
-                System.out.println(array[i]);
             }
             Arrays.sort(array);
             for(int k=0; k<array.length; k++){
@@ -15,7 +12,7 @@ class Solution {
                 }
             }
         }
-        
+
         for(int i=0; i<matrix.length; i++){
             int[] array = matrix[i];
             Arrays.sort(array);
