c5bd6cf791fe27d8d4cfb5fb9b94e18015cbc6fc
İrem Özdemir <iremozdemirwww3@gmail.com>
2025-01-25 14:01:56 +0300

    Time: 0 ms (100.00%) | Memory: 40.5 MB (57.94%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        if(num == 0){
+            return true;
+        }
+        else if(num % 10 == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
