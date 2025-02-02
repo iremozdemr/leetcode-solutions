46cfa66edef1a66c257c8600f3757fd672c462a4
İrem Özdemir <iremozdemirwww3@gmail.com>
2025-02-02 23:08:40 +0300

    Time: 1 ms (18.01%) | Memory: 40.9 MB (20.31%) - LeetSync

---
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countOrders(int n) {
+        int elementCount = n*2;
+        double divide = Math.pow(2,n);
+        //divide = divide % ((long) (Math.pow(10,9)) + 7);
+        int result = factorial(elementCount,divide);
+
+        return result;
+    }
+
+    public int factorial(int number,double divide) {
+        double result = 1;
+
+        for (int factor = 2; factor <= number; factor++) {
+            if(divide >= 2 && factor % 2 == 0){
+                result = result % (Math.pow(10,9) + 7);
+                result *= factor / 2;
+                divide = divide / 2;
+                result = result % (Math.pow(10,9) + 7);
+            }
+            else{
+                result = result % (Math.pow(10,9) + 7);
+                result *= factor;
+                result = result % (Math.pow(10,9) + 7);
+            }
+        }
+
+        result = result % (Math.pow(10,9) + 7);
+        return (int) result;
+    }
+}
